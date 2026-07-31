d299f73e431c8122d3dbbf4f30094f4682c24b93
Yass-Rext <rextyass@gmail.com>
2026-07-31 23:45:49 +0000

    Correction et stabilisation de spark streaming

--- spark/config.py
+++ spark/config.py
@@ -1,19 +1,30 @@
+# config.py
+# import os
+
 APP_NAME = "WaveGuardDetector"
 
+# ============================================================
 # Kafka
+# ============================================================
 KAFKA_BOOTSTRAP_SERVERS = "kafka:29092"
 
 TRANSACTIONS_TOPIC = "transactions"
-FRAUD_TOPIC = "fraud_alerts"
-AUDIT_TOPIC = "audit_log"
+FRAUD_TOPIC = "fraud-alerts"
+AUDIT_TOPIC = "audit-log"
 
+# ============================================================
 # MinIO
+# ============================================================
 MINIO_ENDPOINT = "http://minio:9000"
 MINIO_ACCESS_KEY = "admin"
 MINIO_SECRET_KEY = "password123"
 
+# Chemins S3 pour le Data Lake
 NORMAL_PATH = "s3a://waveguard/normal"
 FRAUD_PATH = "s3a://waveguard/fraud"
 AUDIT_PATH = "s3a://waveguard/audit"
 
-CHECKPOINT_DIR = "/home/jovyan/checkpoints"
+# ============================================================
+# Checkpoints (gardés en local pour la performance)
+# ============================================================
+CHECKPOINT_DIR = "/tmp/waveguard_checkpoints"
--- spark/storage.py
+++ spark/storage.py
@@ -9,45 +9,36 @@ from config import (
 
 
 def write_normal_transactions(df: DataFrame):
-
     return (
         df.writeStream
         .format("parquet")
         .option("path", NORMAL_PATH)
-        .option(
-            "checkpointLocation",
-            f"{CHECKPOINT_DIR}/normal"
-        )
+        .option("checkpointLocation", f"{CHECKPOINT_DIR}/normal")
         .outputMode("append")
+        .trigger(processingTime="60 seconds")  # ✅ Ajouté
         .start()
     )
 
 
 def write_fraud_transactions(df: DataFrame):
-
     return (
         df.writeStream
         .format("parquet")
         .option("path", FRAUD_PATH)
-        .option(
-            "checkpointLocation",
-            f"{CHECKPOINT_DIR}/fraud"
-        )
+        .option("checkpointLocation", f"{CHECKPOINT_DIR}/fraud")
         .outputMode("append")
+        .trigger(processingTime="60 seconds")  # ✅ Ajouté
         .start()
     )
 
 
 def write_audit_logs(df: DataFrame):
-
     return (
         df.writeStream
         .format("parquet")
         .option("path", AUDIT_PATH)
-        .option(
-            "checkpointLocation",
-            f"{CHECKPOINT_DIR}/audit"
-        )
+        .option("checkpointLocation", f"{CHECKPOINT_DIR}/audit")
         .outputMode("append")
+        .trigger(processingTime="60 seconds")  # ✅ Ajouté
         .start()
     )
